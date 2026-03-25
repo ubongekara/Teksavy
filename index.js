93b9a2564d5c3ee378c95606998f048f6e2fd55d
Ubong Ekara <ubongekara@gmail.com>
2026-03-25 10:21:41 -1100

    Add section animation and layout changes

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const menuBtn = document.getElementById("menubtn");
 const hiddenMenu = document.getElementById("hiddenmenu");
-const toTopBtn = document.getElementById("toTop")
+const toTopBtn = document.getElementById("toTop");
+
 
 menuBtn.addEventListener("click", ()=> {
    hiddenMenu.classList.toggle("opacity-0");
@@ -24,3 +25,16 @@ toTopBtn.addEventListener("click", ()=> {
       behavior: "smooth"
    })
 })
+
+      
+document.addEventListener("DOMContentLoaded", ()=> {
+   const sections = document.querySelectorAll(".fade-in");
+   sections.forEach((section, index) => {
+      section.style.transitionDelay = `${index * 600}ms`;
+      setTimeout(() => {
+         section.classList.remove("opacity-0", "translate-y-10");
+          section.classList.add("opacity-100", "translate-y-0");
+      },);
+   })
+})
+
